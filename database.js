c77d073721c411953377aaa540bb687cff685561
ezeAng <65171471+ezeAng@users.noreply.github.com>
2023-11-24 11:51:03 +0800

    first commit

---
+++ database.js
@@ -0,0 +1,17 @@
+import mysql from 'mysql2';
+
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
+}).promise();
+
+
+async function getAllUsers() {
+  const [rows] = await pool.query("SELECT * FROM users");
+  return rows;
+}
+
+
+
